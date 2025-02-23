3c258b86d1e235f8ac47516794d7a061c5ae6731
hruskobur <hrusko.bur@gmail.com>
2025-02-23 14:50:29 +0100

    small refactoring

--- client/src/game/entities/token.js
+++ client/src/game/entities/token.js
@@ -1,6 +1,4 @@
 import { TokenRenderableComponent } from '../components/renderable/token.js';
-import { ActionComponent } from '../components/action/action.js';
-import { CurrentActionComponent } from '../components/action/current.js';
 
 class TokenEntity {
     /**
@@ -8,29 +6,8 @@ class TokenEntity {
      */
     renderable;
 
-    /**
-     * @type {Array<ActionComponent>}
-     */
-    actions;
-
-    /**
-     * @type {CurrentActionComponent}
-     */
-    current;
-
     constructor () {
         this.renderable = new TokenRenderableComponent();
-
-        this.actions = [
-            new ActionComponent('cast_and_generate', 1000),
-            new ActionComponent('cast_and_generate', 1000),
-            new ActionComponent('cast_and_generate', 1000),
-            new ActionComponent('cast_and_generate', 1000),
-            new ActionComponent('cast_and_generate', 1000),
-            new ActionComponent('generate', 0),
-            new ActionComponent('cooldown', 5000),
-        ];
-        this.current = new CurrentActionComponent();
     }
 }
 
